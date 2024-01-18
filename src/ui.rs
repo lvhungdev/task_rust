f2c2604b1f161fb0207353363b56428e408b58d0
lvhung.dev <lvhung.dev@gmail.com>
2024-01-19 00:30:44 +0700

    Implement UI

--- src/ui.rs
+++ src/ui.rs
@@ -4,11 +4,25 @@ pub struct UI;
 
 impl UI {
     pub fn display_tasks(tasks: &[Task]) {
-        println!("Id Description");
-        println!("-- -----------");
+        // let tasks_len: usize = tasks.len().to_string().len();
+        let tasks_len: usize = (tasks.len() * 1000).to_string().len();
+        let space: String = if tasks_len <= 2 {
+            "".to_string()
+        } else {
+            (0..tasks_len - 2).map(|_| " ").collect()
+        };
+
+        println!("Id {} Description", space);
+        println!("-- {} -----------", space);
 
         for (index, task) in tasks.iter().enumerate() {
-            println!("{}  {}", index + 1, task.name);
+            let index = index * 1000;
+            let id_len: usize = index.to_string().len();
+            let space_to_fill: usize = tasks_len - id_len;
+
+            let inner_space: String = (0..space_to_fill).map(|_| " ").collect();
+
+            println!("{}{}  {}", index + 1, inner_space, task.name);
         }
     }
 }
